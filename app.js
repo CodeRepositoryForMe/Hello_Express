854a7b344de7f8ccbc5f7b727098c8f9c31ceffe
SujayM <SujayMane@Hotmail.com>
2020-11-21 01:06:54 +0530

    Added differen Routes

--- app.js
+++ app.js
@@ -4,6 +4,26 @@ const express = require('express');
 
 const app = express();
 
+app.use('/',(req, res, next) => {
+    console.log("This always Executes !!!");
+    next();
+});
+
+app.use('/catelog',(req, res, next) =>{
+    console.log("This is catelog !!!");
+    res.send("<h1>Product catelog page</h1>");
+});
+
+app.use('/user',(req,res,next) =>{
+    console.log("This is user !!!");
+    res.send("<h1>This users page</h1>")
+})
+
+app.use('/',(req,res,next) => {
+    console.log("This is for default route");
+    res.send("<h1> This is from Express for default route</h1>")
+});
+
 const server = http.createServer(app);
 
 server.listen(3000, function(){
