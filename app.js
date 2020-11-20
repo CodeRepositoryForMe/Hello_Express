a4d800868c5b51a4ebf170af529858efe8e63b59
SujayM <SujayMane@Hotmail.com>
2020-11-21 01:27:07 +0530

    Added body parser

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 const http = require('http');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use('/',(req, res, next) => {
     console.log("This always Executes !!!");
+    console.log(req.body);
     next();
 });
 
 app.use('/catelog',(req, res, next) =>{
     console.log("This is catelog !!!");
     res.send("<h1>Product catelog page</h1>");
+});
+
+app.use(('/product'), (req, res, next) => {
+    console.log("This is product page!!!");
+    res.send('<form method="POST", action="./catelog"><input type="Text" name="Title"><button type="Submit">Add product</button></form>');
 });
 
 app.use('/user',(req,res,next) =>{
