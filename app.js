b7846dca3a731d4d3238fcd008d6d937dd4b167c
SujayM <SujayMane@Hotmail.com>
2020-11-21 02:22:36 +0530

    Added Page not found functionality

--- app.js
+++ app.js
@@ -23,6 +23,12 @@ app.use(shopRouter);
 app.use(userRouter);
 app.use(defaultRoute);
 
+app.use((req,res,next)=>{
+    res.status(404).send("<h1>Page Not Found!!!!</h1>")
+});
+
+
+
 const server = http.createServer(app);
 
 server.listen(3000, function(){
--- routes/default.js
+++ routes/default.js
@@ -3,7 +3,7 @@ const { route } = require('./admin');
 
 const router = express.Router();
 
-router.use('/',(req,res,next) => {
+router.get('/',(req,res,next) => {
     console.log("default route !!!");
     res.send("<h1>Default Route of this project!!</h1>");
 });
