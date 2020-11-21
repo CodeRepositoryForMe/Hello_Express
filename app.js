448acadb7c1f6057046d2f3bef2b2579ca342cf5
SujayM <SujayMane@Hotmail.com>
2020-11-21 10:23:48 +0530

    Added page not found html

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -24,7 +25,8 @@ app.use('/user/',userRouter);
 app.use(defaultRoute);
 
 app.use((req,res,next)=>{
-    res.status(404).send("<h1>Page Not Found!!!!</h1>")
+    //res.status(404).send("<h1>Page Not Found!!!!</h1>")
+    res.status(404).sendFile(path.join(__dirname,'views','pageNotFound.html'));
 });
 
 
