473c8ea4cf46fcb289e65ba687995759ecda6a82
SujayM <SujayMane@Hotmail.com>
2020-11-21 10:36:46 +0530

    Refactoring code by adding Root Path

--- routes/admin.js
+++ routes/admin.js
@@ -4,17 +4,19 @@ const express = require('express');
 
 const router = express.Router();
 
+const rootPath = require('../util/path');
+
 
 router.post(('/product'),(req,res,next)=>{
     console.log("Product Added !!!");
     //res.redirect("/catelog");
-    res.sendFile(path.join(__dirname,'..','views','catelog.html'));
+    res.sendFile(path.join(rootPath,'views','catelog.html'));
 });
 
 router.use(('/product'), (req, res, next) => {
     console.log("This is product page!!!");
     //res.send('<form method="POST", action="./product"><input type="Text" name="Title"><button type="Submit">Add product</button></form>');
-    res.sendFile(path.join(__dirname,'..','views','addproduct.html'));
+    res.sendFile(path.join(rootPath,'views','addproduct.html'));
 });
 
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -2,11 +2,13 @@ const path = require('path');
 
 const express = require('express');
 
+const rootPath = require('../util/path');
+
 const router = express.Router();
 
 router.use('/catelog',(req, res, next) =>{
     console.log("This is catelog !!!");
-    res.sendFile(path.join(__dirname,'..','views','catelog.html'));
+    res.sendFile(path.join(rootPath,'views','catelog.html'));
 });
 
 module.exports = router;
