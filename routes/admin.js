a4eeb4c84acbb401a2f9d0c760c9a4c1a3246659
SujayM <SujayMane@Hotmail.com>
2020-11-21 10:01:38 +0530

    Added HTML files and invoke it with Path node lib

--- routes/admin.js
+++ routes/admin.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
@@ -5,12 +7,14 @@ const router = express.Router();
 
 router.post(('/product'),(req,res,next)=>{
     console.log("Product Added !!!");
-    res.redirect("/catelog");
+    //res.redirect("/catelog");
+    res.sendFile(path.join(__dirname,'../','views','catelog.html'));
 });
 
 router.use(('/product'), (req, res, next) => {
     console.log("This is product page!!!");
-    res.send('<form method="POST", action="./product"><input type="Text" name="Title"><button type="Submit">Add product</button></form>');
+    //res.send('<form method="POST", action="./product"><input type="Text" name="Title"><button type="Submit">Add product</button></form>');
+    res.sendFile(path.join(__dirname,'../','views','addproduct.html'));
 });
 
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,12 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
 router.use('/catelog',(req, res, next) =>{
     console.log("This is catelog !!!");
-    res.send("<h1>Product catelog page</h1>");
+    res.sendFile(path.join(__dirname,'../','views','catelog.html'));
 });
 
 module.exports = router;
