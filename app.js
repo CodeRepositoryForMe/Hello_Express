2a3ec1c9a0750ea3717c72e5372a399f89d7387d
SujayM <SujayMane@Hotmail.com>
2020-11-26 01:02:46 +0530

    Added common master.css for all views

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const defaultRoute = require("./routes/default");
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,'public')));
 
 app.use('/',(req, res, next) => {
     console.log("This always Executes !!!");
